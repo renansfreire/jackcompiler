c7c26e7a41fa2490cacb865f6ddf414ca9c73dcb
Renan Freire <renan.freire@icloud.com>
2025-02-25 13:38:09 -0300

    Teste reconhecimento de números inteiros

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,19 +1,14 @@
 package br.ufma.ecp;
 
 import static br.ufma.ecp.token.TokenType.*;
-
-
-
 import br.ufma.ecp.token.Token; 
 
 public class App 
 {
-
-    
     public static void main( String[] args )
     {
 
-    
+    /*
         String input = "45  + if - \"ola\" laranja 876";
         Scanner scan = new Scanner (input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
@@ -40,5 +35,9 @@ public class App
         Token tk = new Token(NUMBER, "42");
         System.out.println(tk);
         */
+
+        String input = "289";
+        Scanner scan = new Scanner(input.getBytes());
+        System.out.println(scan.nextToken());
     }
 }
